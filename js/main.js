472e51d2658830a85e626369e90ea4afac03fdf1
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2020-03-06 03:01:49 -0600

    Updated varibles.scss with colors and font types; Finished navbar with dropdown onhover; Created img directory; Created js directory; In scss/abstracts created animations and mixins files; In scss/components created buttons, jumbotron and top-header files; Implemented GSAP animations; Header and Main are now complete;

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// ---- Navbar dropdown JS
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+//---------------
+// ---- GSAP JS
+var timeline3 = new TimelineMax()
+  .from("aside", {
+    duration: 1,
+    delay: 1,
+    x: -150,
+    opacity: 0
+  })
+  .from(".fade-down", { opacity: 0, duration: 1, y: -50 });
