f67b9b8a215bf5c21194739d0c90d66ddd3f18b4
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2020-03-06 17:07:23 -0600

    Services section added to index.html; Animations already implemented; Added service-1.jpg, service-2.png, services-quote.png; Created card sass file

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(window).on("load resize", function() {
   }
 });
 //---------------
-// ---- GSAP JS
+// ---- GSAP JS for Jumbotron
 var timeline3 = new TimelineMax()
   .from("aside", {
     duration: 1,
@@ -34,3 +34,24 @@ var timeline3 = new TimelineMax()
     opacity: 0
   })
   .from(".fade-down", { opacity: 0, duration: 1, y: -50 });
+
+// ----  JS for Services
+function updatePercentage() {
+  tl.progress();
+  console.log(tl.progress());
+}
+const controller = new ScrollMagic.Controller();
+
+var tl = new TimelineMax({ onUpdate: updatePercentage });
+tl.from(".services-img--1", 1, { x: -200, opacity: 0 });
+tl.from(".services-img--2", 1, { x: 200, opacity: 0 });
+
+tl.from(".services__container", 0.5, { x: 200, opacity: 0 });
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".services",
+  triggerHook: "onLeave",
+  duration: "90%"
+})
+  .setPin(".services")
+  .setTween(tl)
+  .addTo(controller);
