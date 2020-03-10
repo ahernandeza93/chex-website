cc50dbf5f70b3bfaba5d77475ad6303f65cf0775
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2020-03-09 21:11:10 -0600

    Added animation in information container; Added new timeline in main.js

--- js/main.js
+++ js/main.js
@@ -38,25 +38,36 @@ var timeline3 = new TimelineMax()
     x: -150,
     opacity: 0
   })
+
   .from(".fade-down", { opacity: 0, duration: 1, y: -50 });
 
-// ----  JS for Services
-function updatePercentage() {
-  tl.progress();
-  console.log(tl.progress());
+if ($(window).width() >= 1024) {
+  // ----  JS for Services
+  function updatePercentage() {
+    tl.progress();
+  }
+  const controller = new ScrollMagic.Controller();
+
+  var tl = new TimelineMax({ onUpdate: updatePercentage });
+  tl.from(".services-img--1", 1, { x: -200, opacity: 0 });
+  tl.from(".services-img--2", 1, { x: 200, opacity: 0 });
+  tl.from(".services__container", 0.5, { x: 200, opacity: 0 });
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".services",
+    triggerHook: "onLeave",
+    duration: "90%"
+  })
+    .setPin(".services")
+    .setTween(tl)
+    .addTo(controller);
+
+  var tl2 = new TimelineMax();
+  tl2.from(".information__container", 0.5, { x: -200, opacity: 0 });
+  const scene2 = new ScrollMagic.Scene({
+    triggerElement: ".information",
+    triggerHook: "onEnter",
+    duration: "90%"
+  })
+    .setTween(tl2)
+    .addTo(controller);
 }
-const controller = new ScrollMagic.Controller();
-
-var tl = new TimelineMax({ onUpdate: updatePercentage });
-tl.from(".services-img--1", 1, { x: -200, opacity: 0 });
-tl.from(".services-img--2", 1, { x: 200, opacity: 0 });
-
-tl.from(".services__container", 0.5, { x: 200, opacity: 0 });
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".services",
-  triggerHook: "onLeave",
-  duration: "90%"
-})
-  .setPin(".services")
-  .setTween(tl)
-  .addTo(controller);
